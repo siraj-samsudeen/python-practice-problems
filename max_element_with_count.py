6c4b403a6d918b7d039e8fb094685c7120b47d23
Siraj Samsudeen <mailsiraj@gmail.com>
2021-10-06 14:55:29 +0530

    fixed an error in a test case
    and made the return value for an empty array as just None.

--- max_element_with_count.py
+++ max_element_with_count.py
@@ -1,5 +1,5 @@
 def test():
     assert max_element_with_count([1, 2, 3]) == (3, 1)
-    assert max_element_with_count([1, 2, 3, 2]) == (2, 2)
+    assert max_element_with_count([3, 2, 3, 2]) == (3, 2)
     assert max_element_with_count([1]) == (1, 1)
-    assert max_element_with_count([]) == (None, 0)
+    assert max_element_with_count([]) == None
